da52ae9f2cf903f5c38898daf6083e50e396e1ac
Artiom Net <neartiom@yande.ru>
2024-09-20 22:53:35 +0400

    done

---
+++ src/main/java/ru/netology/services/FreelanceService.java
@@ -0,0 +1,20 @@
+package ru.netology.services;
+
+public class FreelanceService {
+    public int calculate(int income, int expenses, int threshold) {
+
+        int balance = 0;
+        int counter = 0;
+        for (int i = 0; i < 12; i++) {
+            if (balance >= threshold) {
+                balance -= expenses;
+                balance /= 3;
+                counter++;
+            } else {
+                balance += income;
+                balance -= expenses;
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/test/java/ru/netology/services/FreelanceServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FreelanceServiceTest {
+
+    @Test
+    public void FreelanceServiceTestOption1() {
+        FreelanceService service = new FreelanceService();
+        int income = 10_000;
+        int expenses = 3_000;
+        int threshold = 20_000;
+        int actual = service.calculate(income, expenses, threshold);
+        int expected = 3;
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void FreelanceServiceTestOption2() {
+        FreelanceService service = new FreelanceService();
+        int income = 100_000;
+        int expenses = 60_000;
+        int threshold = 150_000;
+        int actual = service.calculate(income, expenses, threshold);
+        int expected = 2;
+        Assertions.assertEquals(actual, expected);
+    }
+
+}
