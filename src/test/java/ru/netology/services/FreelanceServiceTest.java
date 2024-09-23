d6592fab1625272ab138894ab1de94541f015eda
Artiom Net <neartiom@yande.ru>
2024-09-24 00:31:14 +0400

    Patch

--- src/test/java/ru/netology/services/FreelanceServiceTest.java
+++ src/test/java/ru/netology/services/FreelanceServiceTest.java
@@ -31,4 +31,11 @@ class FreelanceServiceTest {
         Assertions.assertEquals(actual, expected);
     }
 
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/Test.csv")
+    public void FreelanceServiceTestParams(int income, int expenses, int threshold, int expected) {
+        FreelanceService service = new FreelanceService();
+        int actual = service.calculate(income, expenses, threshold);
+        Assertions.assertEquals(actual, expected);
+    }
 }
